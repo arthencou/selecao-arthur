27f728c23db96317ab331e9aa3798d090262c0f9
Arthur Henrique Coutinho <arthencou@gmail.com>
2018-04-13 19:53:16 -0300

    Cria projeto maven

---
+++ src/main/java/com/atos/selecaoarthur/SelecaoArthurApplication.java
@@ -0,0 +1,12 @@
+package com.atos.selecaoarthur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaoArthurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelecaoArthurApplication.class, args);
+    }
+}
